5dd79bcfc48b5f8e9caba786503165ff75ce4175
kijunHan <gksrlwns96@naver.com>
2024-04-06 12:16:22 +0900

    Update : Add Asset & Player Input
    - 병력(솔져), 장애물, 스테이지 배경, 좀비(적) Asset 추가
    - new InputSystem 적용 (마우스, 방향키, 터치)

---
+++ Assets/_Project/Scripts/Player_Input.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Input : MonoBehaviour
+{
+    public Vector2 inputVec;
+    Rigidbody rigid;
+    public float speed;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 nextVec = inputVec.normalized * Time.deltaTime * speed;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    void OnMove(InputValue input)
+    {
+        inputVec = input.Get<Vector2>();
+    }
+}
