503a7d6bf1a0af5ca21fefcfe9de4dec81ec9b0d
kijunHan <gksrlwns96@naver.com>
2024-07-14 23:37:12 +0900

    Update : Solider Animator
    한기준

--- Assets/_Project/Scripts/Soldier/SoldierAnimator.cs
+++ Assets/_Project/Scripts/Soldier/SoldierAnimator.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class SoldierAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isAttacking;
+    Dictionary<SecondsType, WaitForSeconds> waitForSecondsDict;
+    Animator animator;
+    enum SecondsType { AttackSpeed, Attack_Anim};
+
+
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        waitForSecondsDict = new Dictionary<SecondsType, WaitForSeconds>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInitAnimation()
     {
-        
+        animator.SetBool("isLive", true);
+        animator.SetBool("isVictory", false);
+    }
+
+    public void OnMove(Vector3 moveVec)
+    {
+        animator.SetFloat("Speed", moveVec.magnitude);
+    }
+
+    public void OnAttack()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    public void OnRevive()
+    {
+        animator.SetBool("isLive", true);
+    }
+    IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+        animator.SetTrigger("Attack");
+
+        //Debug.Log("기본 공격 사용");
+        yield return waitForSecondsDict[SecondsType.Attack_Anim];
+
+        yield return waitForSecondsDict[SecondsType.AttackSpeed];
+        isAttacking = false;
+        //animator.SetBool("isAttacking", isAttacking);
+    }
+    public void OnDamaged()
+    {
+        if (!isAttacking)
+        {
+            animator.SetTrigger("Hurt");
+        }
+    }
+    public void OnDead()
+    {
+        animator.SetBool("isLive", false);
+        animator.SetTrigger("Dead");
+    }
+    public void OnVictory()
+    {
+        animator.SetBool("isVictory", true);
     }
 }
